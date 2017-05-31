4f8651e5c36d27d312d245237e399fded743061c
shubhamkumarnayak <shubham556@gmail.com>
2017-06-01 01:33:04 +0530

    Update ChocolateyPackageInstaller.ps1

--- artifacts/vs2013community-windows/ChocolateyPackageInstaller.ps1
+++ artifacts/vs2013community-windows/ChocolateyPackageInstaller.ps1
@@ -223,7 +223,7 @@ function InstallPackages
         WriteLog "Installing package: $package ..."
 
         # Install git via chocolatey.
-        choco install $package --force --yes --acceptlicense --verbose --allow-empty-checksums | Out-Null  
+        choco install $package --version 1.0.1 --force --yes --acceptlicense --verbose --allow-empty-checksums | Out-Null  
         if (-not $?)
         {
             $errMsg = 'Installation failed. Please see the chocolatey logs in %ALLUSERSPROFILE%\chocolatey\logs folder for details.'
